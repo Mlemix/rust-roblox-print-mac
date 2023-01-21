43d58561cbf03634be2b10c24b11a31f68736b2d
Mlemix <mlemixbusiness@gmail.com>
2023-01-21 11:51:32 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
-#[macro_use]
-extern crate ctor;
-
-use std::thread;
+use ctor::ctor;
 use std::ffi::CString;
 use std::os::raw::c_char;
 use std::mem::transmute;
@@ -19,22 +16,24 @@ enum Colours {
     Error
 }
 
-unsafe fn entry() {
+ fn entry() {
     // address is for version-c7d5978a8bea4dfd
-    let intermediate_ptr = (_dyld_get_image_vmaddr_slide(0) + 0x100b47aaa) as *const ();
-    let print = transmute::<*const (), extern "C" fn (Colours, *const c_char, ...) -> ()>(intermediate_ptr);
+    let print = unsafe { 
+        let intermediate_ptr = (_dyld_get_image_vmaddr_slide(0) + 0x100b47aaa) as *const ();
+        transmute::<*const (), extern "C" fn (Colours, *const c_char, ...) -> ()>(intermediate_ptr)
+    };
 
     let mut print_msg = CString::new("this is white").unwrap();
     print(Colours::Regular, print_msg.as_ptr());
-    thread::sleep(Duration::from_millis(100));
+    std::thread::sleep(Duration::from_millis(100));
 
     print_msg = CString::new("this is blue").unwrap();
     print(Colours::Info, print_msg.as_ptr());
-    thread::sleep(Duration::from_millis(100));
+    std::thread::sleep(Duration::from_millis(100));
 
     print_msg = CString::new("this is yellow").unwrap();
     print(Colours::Warn, print_msg.as_ptr());
-    thread::sleep(Duration::from_millis(100));
+    std::thread::sleep(Duration::from_millis(100));
 
     print_msg = CString::new("this is red").unwrap();
     print(Colours::Error, print_msg.as_ptr());
@@ -42,5 +41,5 @@ unsafe fn entry() {
 
 #[ctor]
 unsafe fn constructor() {
-    thread::spawn(move || entry());
+    std::thread::spawn(move || entry());
 }
